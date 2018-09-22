8034b78b59296ee9859fba5753938f22773a852f
Naman Saxena <naman.saxena@learner.manipal.edu>
2018-09-23 00:05:08 +0530

    primes using conventional method

--- PSUC/lab6/p5.cpp
+++ PSUC/lab6/p5.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
@@ -9,16 +10,27 @@ int main(int argc, char const *argv[])
 	int n;
 	cout << "Enter the limit for printing Primes: ";
 	cin >> n;
-	if (i<2) {
+	if ( n < 2 ) {
 		cout << "No primes found";
 	}
 	else {
-		if ( n == 2)
-		{
+		if ( n == 2) {
 			cout << "Primes: 2";
 		}
+		else {
+			for (int i = 2; i < n; ++i) {
+				int flag = 0;
+				for (int j = 2; j <= sqrt(i); ++j) {
+					if( (i%j) == 0 ) {
+						flag = 1;
+						break;
+					}
+				}
+				if ( flag == 0 )
+					cout << i << " ";
+			}
+		}
 	}
-	for (int i=2)
 
 	cout << endl;
 	return 0;
