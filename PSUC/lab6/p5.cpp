20b061eafd710d7cd2f2f34b54978862b492518d
Naman Saxena <naman.saxena@learner.manipal.edu>
2018-09-22 23:59:31 +0530

    primes

--- PSUC/lab6/p5.cpp
+++ PSUC/lab6/p5.cpp
@@ -2,10 +2,24 @@
 
 using namespace std;
 
-// Sieve of Eratosthenes
+// Finding prime numbers
 
 int main(int argc, char const *argv[])
 {
-	
+	int n;
+	cout << "Enter the limit for printing Primes: ";
+	cin >> n;
+	if (i<2) {
+		cout << "No primes found";
+	}
+	else {
+		if ( n == 2)
+		{
+			cout << "Primes: 2";
+		}
+	}
+	for (int i=2)
+
+	cout << endl;
 	return 0;
 }
